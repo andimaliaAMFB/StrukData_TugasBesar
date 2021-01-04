13ee899eb818f80e668af42fa79d556406dda56f
DESKTOP-9JODO8N\USER <011007andimalia@gmail.com>
2021-01-04 09:41:26 +0700

    no message

--- StrukData_Tugas Besar/src/LihatFilm.java
+++ StrukData_Tugas Besar/src/LihatFilm.java
@@ -5,7 +5,9 @@ public class LihatFilm {
     static String [] Genre = {"G1", "G2","G3"};
     static String [] Judul = {"T1", "T2", "T3"};
     public static Hashtable Film = new Hashtable();
+    public static Hashtable NoFilm = new Hashtable();
     public static void FilmSorted(){
+    //public static void main(String[]args){
         for(int i=0;i<Genre.length;i++)
         {
             Hashinput(Judul[i],Genre[i]);
@@ -17,8 +19,12 @@ public class LihatFilm {
         for(Map.Entry<String,String> SF : Sorted_film.entrySet())
         {
             System.out.println(no+". "+SF.getKey()+"\n  Genre: "+SF.getValue());
+            NoFilm.put(no,SF.getKey());
             no++;
         }
+        
+        //example output for Nofilm Hashtable
+        //System.out.println(NoFilm.get(1));
     }
     public static void Hashinput(String jdl, String gnr){
         Film.put(jdl,gnr);
